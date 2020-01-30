85045948375d0edca6ea28521bc7b815bdea8c58
Umpalink <60490201+Umpalink@users.noreply.github.com>
2020-01-30 15:24:30 -0700

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,48 @@
+
+
+import java.util.*;
+import java.io.*;
+
+
+public class Player {
+
+    private String name;
+    private int health;
+    private int damage;
+
+    public Player(String n){
+        name = n;
+    }
+
+    public Player() {
+
+    }
+
+    public void PlayerHealth(int h){
+        health= h;
+    }
+
+    public void PlayerDmg(int d){
+        damage = d;
+    }
+    public int  GetHealth( ){
+        return health;
+    }
+    public String GetName(){
+        return name;
+    }
+
+    public int GetDamage(){
+        return damage;
+    }
+
+    public void TakeDamage(int d){
+        health -= d;
+    }
+    public int Attack(){
+        return damage;
+    }
+    public void GainHealth(int z){
+        health +=z;
+    }
+}
